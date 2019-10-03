58b2adc64c10dc59187e3cd5b4cf9a89c896f7cc
Frederick Lee <frederick.gfl@gmail.com>
2019-10-03 07:00:17 +0000

    reorganized conditionals

--- conditionals.js
+++ conditionals.js
@@ -10,16 +10,12 @@ if (!raining) {
 
 if (cold) {
   console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
 }
 
 console.log("Now you're ready to go outside!");
 
-//---------------
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-} else {
-  console.log("Short sleeves are fine.");
-}
 //---------------
 const temperature = 40;
 
@@ -27,15 +23,13 @@ if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
+} else if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
 } else {
   console.log("Short sleeves are fine.");
 }
 
 console.log("Now you're ready to go outside!");
-//----------------
-if (temperature < -40 || temperature > 40) {
-  console.log("Maybe going outside isn't such a great idea…");
-}
 
 //----------------
 const isCitizen = true;
