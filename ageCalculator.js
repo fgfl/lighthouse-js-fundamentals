b6381ab0556cec45ca3865134f4acc538cda625e
Frederick Lee <frederick.gfl@gmail.com>
2019-10-05 21:42:30 +0000

    add packing-list example

---
+++ ageCalculator.js
@@ -0,0 +1,8 @@
+function ageCalculator(name, yearOfBrith, currentYear) {
+  
+  return (name + ' is ' + (currentYear - yearOfBrith) + ' years old.');
+
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
